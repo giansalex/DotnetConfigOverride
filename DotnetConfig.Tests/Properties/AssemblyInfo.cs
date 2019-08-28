f85ec89a5919d68f13362305fd88d3a8e16edebf
Giancarlos Salas <giansalex@gmail.com>
2019-08-28 10:10:55 -0500

    add test project

---
+++ DotnetConfig.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DotnetConfig.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DotnetConfig.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d3412dae-90fd-48d1-a7cf-abe3b71fd1a8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DotnetConfig.Tests/WebConfigTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace DotnetConfig.Tests
+{
+    [TestFixture]
+    public class WebConfigTests
+    {
+        [Test]
+        public void OverrideEmptySections()
+        {
+        }
+    }
+}
