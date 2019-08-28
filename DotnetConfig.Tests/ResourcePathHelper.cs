fbacd9fc551000de29fe7cdace51db990e0cff8f
Giancarlos Salas <giansalex@gmail.com>
2019-08-28 10:38:32 -0500

    add webconfig tests

---
+++ DotnetConfig.Tests/ResourcePathHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace DotnetConfig.Tests
+{
+    internal static class ResourcePathHelper
+    {
+        public static string GetPath(string name)
+        {
+            string assemblyFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? throw new InvalidOperationException();
+
+            return Path.Combine(assemblyFolder , "Resources", name);
+        }
+    }
+}
--- DotnetConfig.Tests/WebConfigTests.cs
+++ DotnetConfig.Tests/WebConfigTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System.Configuration;
+using static DotnetConfig.Tests.ResourcePathHelper;
+using NUnit.Framework;
 
 namespace DotnetConfig.Tests
 {
@@ -8,6 +10,29 @@ namespace DotnetConfig.Tests
         [Test]
         public void OverrideEmptySections()
         {
+            var config = GetPath("web_empty_sections.config");
+            var team = ConfigurationManager.AppSettings["Team"];
+            var @default = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
+
+            WebConfig.Override(config);
+            
+            Assert.AreEqual(team, ConfigurationManager.AppSettings["Team"]);
+            Assert.AreEqual(@default, ConfigurationManager.ConnectionStrings["Default"].ConnectionString);
+        }
+
+        [Test]
+        public void OverrideWithValues()
+        {
+            var config = GetPath("web_with_values.config");
+            var team = ConfigurationManager.AppSettings["Team"];
+            var @default = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
+
+            WebConfig.Override(config);
+            
+            Assert.AreNotEqual(team, ConfigurationManager.AppSettings["Team"]);
+            Assert.AreNotEqual(@default, ConfigurationManager.ConnectionStrings["Default"].ConnectionString);
+            Assert.AreEqual("B", ConfigurationManager.AppSettings["Team"]);
+            StringAssert.Contains("DB_B", ConfigurationManager.ConnectionStrings["Default"].ConnectionString);
         }
     }
 }
